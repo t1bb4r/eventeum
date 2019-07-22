597c16bb269497c64b2406e367ec3b1f48a7f574
Fernando Llaca <ferllarom@gmail.com>
2019-07-22 23:12:09 +0200

    first draft of sql support

--- core/src/main/java/net/consensys/eventeum/model/TransactionMonitoringSpec.java
+++ core/src/main/java/net/consensys/eventeum/model/TransactionMonitoringSpec.java
@@ -3,12 +3,18 @@ package net.consensys.eventeum.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import org.web3j.crypto.Hash;
 
+@Entity
 @Data
 @NoArgsConstructor
 public class TransactionMonitoringSpec {
 
+    @Id
     private String id;
 
     private TransactionIdentifierType type;
